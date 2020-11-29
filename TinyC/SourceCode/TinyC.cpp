89b76cefc14975ad3cc3744006d7f927e22d6933
HorribleCavalry <HordrenHan@outlook.com>
2020-11-30 02:06:45 +0800

    Use usual pointer.

--- TinyC/SourceCode/TinyC.cpp
+++ TinyC/SourceCode/TinyC.cpp
@@ -5,161 +5,57 @@
 #include<cmath>
 #include <vector>
 #include <stack>
+#include <utility>
 
-int token;
-char* src, *old_src;
-
-const int poolSize = 1024 * 256;
-int line;
-
-int *text,
-*old_text,
-*stack;
-char *data;
-
-int* pc, *bp, *sp, ax, cycle;
-
-enum
-{
-	NONE,LEA, IMM, JMP, CALL, JZ, JNZ, ENT, ADJ, LEV, LI, LC, SI, SC, PUSH,
-	OR, XOR, AND, EQ, NE, LT, GT, LE, GE, SHL, SHR, ADD, SUB, MUL, DIV, MOD,
-	OPEN, READ, CLOS, PRTF, MALC, MSET, MCMP, EXIT
-};
-
-void next()
-{
-	token = *src++;
-}
-
-void expression(int level)
-{
-
-}
-
-void program()
-{
-	next();
-	while (token > 0)
-	{
-		printf("Token is: %c\n", token);
-		next();
-	}
-}
-
-//int eval()
-//{
-//	Instruction op = Instruction::NONE;
-//	int* temp = nullptr;
-//
-//	while (true)
-//	{
-//		op = (Instruction)*pc++;
-//		switch (op)
-//		{
-//		case NONE:
-//			break;
-//		case LEA: {ax = (int)(bp + *pc++); }
-//			break;
-//		case IMM: {ax = *pc; ++pc; }
-//			break;
-//		case JMP: {pc = (int*)*pc; }
-//			break;
-//		case CALL: {--sp; *sp = (int)(pc + 1); pc = (int*)*pc; }
-//			break;
-//		case JZ: {pc = ax ? pc + 1 : (int*)*pc; }
-//			break;
-//		case JNZ: {pc = ax ? (int*)*pc : pc + 1; }
-//			break;
-//		case ENT: {--sp; *sp = (int)bp; bp = sp; sp = sp - *pc++; }
-//			break;
-//		case ADJ: {sp = sp + *pc++; }
-//			break;
-//		case LEV: {sp = bp; bp = (int *)*sp++; pc = (int *)*sp++; }
-//			break;
-//		case LI: {ax = *(int*)ax; }
-//			break;
-//		case LC: {ax = *(char*)ax; }
-//			break;
-//		case SI: {*(int*)*sp = ax; ++sp; }
-//			break;
-//		case SC: {*(char*)*sp = ax, ++sp; }
-//			break;
-//		case PUSH: {--sp; *sp = ax; }
-//			break;
-//		case OR: {ax = *sp++ | ax; }
-//			break;
-//		case XOR: {ax = *sp++ ^ ax; }
-//			break;
-//		case AND: {ax = *sp++ & ax; }
-//			break;
-//		case EQ: {ax = *sp++ == ax; }
-//			break;
-//		case NE: {ax = *sp++ != ax; }
-//			break;
-//		case LT: {ax = *sp++ < ax; }
-//			break;
-//		case GT: {ax = *sp++ > ax; }
-//			break;
-//		case LE: {ax = *sp++ <= ax; }
-//			break;
-//		case GE: {ax = *sp++ >= ax; }
-//			break;
-//		case SHL: {ax = *sp++ << ax; }
-//			break;
-//		case SHR: {ax = *sp++ >> ax; }
-//			break;
-//		case ADD: {ax = *sp++ + ax; }
-//			break;
-//		case SUB: {ax = *sp++ - ax; }
-//			break;
-//		case MUL: {ax = *sp++ * ax; }
-//			break;
-//		case DIV: {ax = *sp++ / ax; }
-//			break;
-//		case MOD: {ax = *sp++ % ax; }
-//			break;
-//		case OPEN: 
-//			break;
-//		case READ:
-//			break;
-//		case CLOS:
-//			break;
-//		case PRTF:
-//			break;
-//		case MALC: {ax = (int)malloc(*sp); }
-//			break;
-//		case MSET: {ax = (int)memset((char*)sp[2], sp[1], *sp); }
-//			break;
-//		case MCMP: {ax = memcmp((char *)sp[2], (char *)sp[1], *sp); }
-//			break;
-//		case EXIT: {printf("Exit(%d)", *sp); return *sp; }
-//			break;
-//		default: {printf("unknown instruction:%d\n", op); return -1; }
-//			break;
-//		}
-//	}
-//}
 
 class TinyCProgram
 {
+public:
+	enum
+	{
+		NONE, LEA, IMM, JMP, CALL, JZ, JNZ, ENT, ADJ, LEV, LI, LC, SI, SC, PUSH,
+		OR, XOR, AND, EQ, NE, LT, GT, LE, GE, SHL, SHR, ADD, SUB, MUL, DIV, MOD,
+		OPEN, READ, CLOS, PRTF, MALC, MSET, MCMP, EXIT
+	};
 private:
 	std::string content;
-	std::stack<long long> heapStack;
 	unsigned long long line;
-	std::vector<long long> memory;
+	unsigned long long stackSize;
+	unsigned long long memorySize;
 
+	long long* stackPointer;
+	long long* stackMemory;
+	long long* programCounter;
+	long long* programMemory;
 	long long ax;
+
+public:
+	TinyCProgram()
+		:line(1), stackSize(1024*1024), memorySize(1024 * 1024)
+	{
+		stackPointer = stackMemory = (long long*)malloc(stackSize);
+		programCounter = programMemory = (long long*)malloc(memorySize);
+	}
+
+	TinyCProgram(unsigned long long _stackSize, unsigned long long _memorySize)
+		:line(1), stackSize(_stackSize), memorySize(_memorySize)
+	{
+		stackPointer = stackMemory = (long long*)malloc(stackSize);
+		programCounter = programMemory = (long long*)malloc(memorySize);
+	}
+
 private:
 	void GenerateInstructions()
 	{
-		memory.push_back(IMM);
-		memory.push_back(30);
-		memory.push_back(PUSH);
-		memory.push_back(IMM);
-		memory.push_back(20);
-		memory.push_back(SUB);
-		memory.push_back(PUSH);
-		memory.push_back(EXIT);
+		int iterMem = 0;
+		programMemory[iterMem++] = IMM;
+		programMemory[iterMem++] = 12;
+		programMemory[iterMem++] = PUSH;
+		programMemory[iterMem++] = IMM;
+		programMemory[iterMem++] = 24;
+		programMemory[iterMem++] = ADD;
+		programMemory[iterMem++] = PUSH;
+		programMemory[iterMem++] = EXIT;
 	}
 
 public:
@@ -182,23 +78,25 @@ public:
 	}
 	void Execute()
 	{
-		for (int pc = 0; pc < memory.size(); ++pc)
+		long long OP;
+		while (programCounter < programMemory + memorySize/sizeof(long long))
 		{
-			switch (memory[pc])
+			OP = *programCounter;
+			switch (OP)
 			{
 			case NONE:
 				break;
 			case LEA: {/*ax = (int)(bp + *pc++); */}
 					  break;
-			case IMM: {++pc; ax = memory[pc]; }
+			case IMM: {++programCounter; ax = *programCounter; }
 					  break;
-			case JMP: {pc = memory[pc + 1]; }
+			case JMP: {programCounter = (long long*)*(programCounter + 1); }
 					  break;
 			case CALL: {/*--sp; *sp = (int)(pc + 1); pc = (int*)*pc; */}
 					   break;
-			case JZ: {pc = ax ? pc + 1 : memory[pc + 1]; }
+			case JZ: {programCounter = ax ? programCounter + 1 : (long long*)*(programCounter + 1); }
 					 break;
-			case JNZ: {pc = ax ? memory[pc + 1] : pc + 1; }
+			case JNZ: {programCounter = ax ? (long long*)*(programCounter + 1) : programCounter + 1; }
 					  break;
 			case ENT: {/*--sp; *sp = (int)bp; bp = sp; sp = sp - *pc++;*/ }
 					  break;
@@ -206,47 +104,47 @@ public:
 					  break;
 			case LEV: {/*sp = bp; bp = (int *)*sp++; pc = (int *)*sp++;*/ }
 					  break;
-			case LI: {ax = memory[ax]; }
+			case LI: {ax = programMemory[ax]; }
 					 break;
-			case LC: {ax = memory[ax]; }
+			case LC: {ax = programMemory[ax]; }
 					 break;
 			case SI: {/**(int*)*sp = ax; ++sp; */}
 					 break;
 			case SC: {/**(char*)*sp = ax, ++sp; */}
 					 break;
-			case PUSH: {heapStack.push(ax);}
+			case PUSH: {*stackPointer = ax; ++stackPointer; }
 					   break;
-			case OR: {ax = heapStack.top() | ax; }
+			case OR: {ax = *(stackPointer-1) | ax; }
 					 break;
-			case XOR: {ax = heapStack.top() ^ ax; }
+			case XOR: {ax = *(stackPointer - 1) ^ ax; }
 					  break;
-			case AND: {ax = heapStack.top() & ax; }
+			case AND: {ax = *(stackPointer - 1) & ax; }
 					  break;
-			case EQ: {ax = heapStack.top() == ax; }
+			case EQ: {ax = *(stackPointer - 1) == ax; }
 					 break;
-			case NE: {ax = heapStack.top() != ax; }
+			case NE: {ax = *(stackPointer - 1) != ax; }
 					 break;
-			case LT: {ax = heapStack.top() < ax; }
+			case LT: {ax = *(stackPointer - 1) < ax; }
 					 break;
-			case GT: {ax = heapStack.top() > ax; }
+			case GT: {ax = *(stackPointer - 1) > ax; }
 					 break;
-			case LE: {ax = heapStack.top() <= ax; }
+			case LE: {ax = *(stackPointer - 1) <= ax; }
 					 break;
-			case GE: {ax = heapStack.top() >= ax; }
+			case GE: {ax = *(stackPointer - 1) >= ax; }
 					 break;
-			case SHL: {ax = heapStack.top() << ax; }
+			case SHL: {ax = *(stackPointer - 1) << ax; }
 					  break;
-			case SHR: {ax = heapStack.top() >> ax; }
+			case SHR: {ax = *(stackPointer - 1) >> ax; }
 					  break;
-			case ADD: {ax = heapStack.top() + ax; }
+			case ADD: {ax = *(stackPointer - 1) + ax; }
 					  break;
-			case SUB: {ax = heapStack.top() - ax; }
+			case SUB: {ax = *(stackPointer - 1) - ax; }
 					  break;
-			case MUL: {ax = heapStack.top() * ax; }
+			case MUL: {ax = *(stackPointer - 1) * ax; }
 					  break;
-			case DIV: {ax = heapStack.top() / ax; }
+			case DIV: {ax = *(stackPointer - 1) / ax; }
 					  break;
-			case MOD: {ax = heapStack.top() % ax; }
+			case MOD: {ax = *(stackPointer - 1) % ax; }
 					  break;
 			case OPEN:
 				break;
@@ -262,14 +160,21 @@ public:
 					   break;
 			case MCMP: {/*ax = memcmp((char *)sp[2], (char *)sp[1], *sp);*/ }
 					   break;
-			case EXIT: {std::cout<<heapStack.top()<<std::endl; return; }
+			case EXIT: {std::cout << *(stackPointer - 1) << std::endl; return; }
 					   break;
-			default: {std::cout<<"unknown instruction:%d\n"<<heapStack.top(); return; }
+			default: {std::cout << "unknown instruction:%d\n" << *(stackPointer - 1); return; }
 					 break;
 			}
+
+			++programCounter;
+
 		}
 	}
-
+	~TinyCProgram()
+	{
+		delete[] stackMemory;
+		delete[] programMemory;
+	}
 };
 
 int main(int argc, char** argv)
@@ -277,37 +182,9 @@ int main(int argc, char** argv)
 	std::string exePath = argv[0];
 	std::string hierarchyPath = exePath.substr(0, exePath.find_last_of('\\') + 1);
 	std::string path = hierarchyPath.append("program.c");
-	TinyCProgram routine;
+	TinyCProgram routine(8 * sizeof(long long), 8 * sizeof(long long));
 	routine.CompileFile(path);
 
 	routine.Execute();
 
-	//line = 1;
-	//src = old_src = (char*)malloc(poolSize);
-	//text = old_text = (int*)malloc(poolSize);
-	//data = (char*)malloc(poolSize);
-
-	//stack = (int*)malloc(poolSize);
-
-	//memset(text, 0, poolSize);
-	//memset(data, 0, poolSize);
-	//memset(stack, 0, poolSize);
-
-	//bp = sp = (int*)((int)stack + poolSize);
-	//ax = 0;
-
-	//int i = 0;
-	//text[i++] = IMM;
-	//text[i++] = 10;
-	//text[i++] = PUSH;
-	//text[i++] = IMM;
-	//text[i++] = 20;
-	//text[i++] = ADD;
-	//text[i++] = PUSH;
-	//text[i++] = EXIT;
-
-	//pc = text;
-	//program();
-
-	//return eval();
 }
