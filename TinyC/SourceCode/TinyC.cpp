6d4daaa771ac239794367b6b4066c82079905396
hanshubin <hanshubin@xd.com>
2020-11-29 22:40:16 +0800

    Trying use C++ implemente basic instruction.

--- TinyC/SourceCode/TinyC.cpp
+++ TinyC/SourceCode/TinyC.cpp
@@ -3,6 +3,8 @@
 #include<string>
 #include <sstream>
 #include<cmath>
+#include <vector>
+#include <stack>
 
 int token;
 char* src, *old_src;
@@ -138,19 +140,141 @@ int eval()
 	}
 }
 
+class TinyCProgram
+{
+private:
+	std::string content;
+	std::stack<int> executeStack;
+	int pc;
+	std::vector<Instruction> instructions;
+
+	int ax;
+private:
+	void GenerateInstructions()
+	{
+
+	}
+
+	void Execute()
+	{
+		for (int pc = 0; pc < instructions.size(); ++pc)
+		{
+			switch (instructions[pc])
+			{
+			case NONE:
+				break;
+			case LEA: {/*ax = (int)(bp + *pc++);*/ }
+					  break;
+			case IMM: {++pc; ax = instructions[pc]; }
+					  break;
+			case JMP: {pc = instructions[pc + 1]; }
+					  break;
+			case CALL: {/*--sp; *sp = (int)(pc + 1); pc = (int*)*pc; */}
+					   break;
+			case JZ: {pc = ax ? pc + 1 : instructions[pc + 1]; }
+					 break;
+			case JNZ: {pc = ax ? instructions[pc + 1] : pc + 1; }
+					  break;
+			case ENT: {/*--sp; *sp = (int)bp; bp = sp; sp = sp - *pc++;*/ }
+					  break;
+			case ADJ: {/*sp = sp + *pc++;*/ }
+					  break;
+			case LEV: {/*sp = bp; bp = (int *)*sp++; pc = (int *)*sp++;*/ }
+					  break;
+			case LI: {ax = instructions[ax]; }
+					 break;
+			case LC: {ax = instructions[ax]; }
+					 break;
+			case SI: {/**(int*)*sp = ax; ++sp; */}
+					 break;
+			case SC: {/**(char*)*sp = ax, ++sp; */}
+					 break;
+			case PUSH: {executeStack.push(ax);}
+					   break;
+			case OR: {ax = executeStack.top() | ax; }
+					 break;
+			case XOR: {ax = executeStack.top() ^ ax; }
+					  break;
+			case AND: {ax = executeStack.top() & ax; }
+					  break;
+			case EQ: {ax = executeStack.top() == ax; }
+					 break;
+			case NE: {ax = executeStack.top() != ax; }
+					 break;
+			case LT: {ax = executeStack.top() < ax; }
+					 break;
+			case GT: {ax = executeStack.top() > ax; }
+					 break;
+			case LE: {ax = executeStack.top() <= ax; }
+					 break;
+			case GE: {ax = executeStack.top() >= ax; }
+					 break;
+			case SHL: {ax = executeStack.top() << ax; }
+					  break;
+			case SHR: {ax = executeStack.top() >> ax; }
+					  break;
+			case ADD: {ax = executeStack.top() + ax; }
+					  break;
+			case SUB: {ax = executeStack.top() - ax; }
+					  break;
+			case MUL: {ax = executeStack.top() * ax; }
+					  break;
+			case DIV: {ax = executeStack.top() / ax; }
+					  break;
+			case MOD: {ax = executeStack.top() % ax; }
+					  break;
+			case OPEN:
+				break;
+			case READ:
+				break;
+			case CLOS:
+				break;
+			case PRTF:
+				break;
+			case MALC: {/*ax = (int)malloc(*sp); */}
+					   break;
+			case MSET: {/*ax = (int)memset((char*)sp[2], sp[1], *sp);*/ }
+					   break;
+			case MCMP: {/*ax = memcmp((char *)sp[2], (char *)sp[1], *sp);*/ }
+					   break;
+			case EXIT: {std::cout<<executeStack.top()<<std::endl; return; }
+					   break;
+			default: {std::cout<<"unknown instruction:%d\n"<<executeStack.top(); return; }
+					 break;
+			}
+		}
+	}
+
+public:
+
+	
+
+	void CompileFile(std::string path)
+	{
+		std::stringstream buffer;
+		std::ifstream inf;
+		inf.open(path);
+		buffer << inf.rdbuf();
+		content = buffer.str();
+		inf.close();
+		std::cout << "Now compiling C file: " << path << std::endl;
+		std::cout << "This content of current file is:" << std::endl;
+		std::cout << content << std::endl;
+
+		std::cout << "Now compiling finished!" << std::endl;
+
+	}
+
+};
+
 int main(int argc, char** argv)
 {
-	std::stringstream buffer;
-	std::string content;
-	std::ifstream inf;
+
 	std::string exePath = argv[0];
 	std::string hierarchyPath = exePath.substr(0, exePath.find_last_of('\\') + 1);
-	std::string targetPath = hierarchyPath.append("program.c");
-	inf.open(targetPath);
-	buffer << inf.rdbuf();
-	content = buffer.str();
-	inf.close();
-	std::cout << content<<std::endl;
+	std::string path = hierarchyPath.append("program.c");
+	TinyCProgram routine;
+	routine.CompileFile(path);
 
 	line = 1;
 	src = old_src = (char*)malloc(poolSize);
